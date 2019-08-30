d378a30a42073bb764cf4f08c3aae0747c7e836a
171240501-Kuang Shulei <923950043@qq.com>
2019-08-30 21:22:33 +0800

    yyy

--- code/main.py
+++ code/main.py
@@ -7,5 +7,5 @@ import network
 
 training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
 net = network.Network([784, 30, 10], cost=network.CrossEntropyCost)
-net.large_weight_initializer()
-net.SGD(training_data, 30, 10, 0.5, evaluation_data=test_data, monitor_evaluation_accuracy=True)
+net.SGD(training_data, 30, 10, 0.5, lmbda = 5.0, evaluation_data=validation_data, monitor_evaluation_accuracy=True,
+        monitor_evaluation_cost=True, monitor_training_accuracy=True,  monitor_training_cost=True)
